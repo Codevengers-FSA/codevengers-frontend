537ae135d1079e70be1a06ac48fb4fe65cf9f3e1
Kelsey Donovan <kelsey.donovan628@gmail.com>
2024-12-10 16:29:29 -0700

    Update AccountDetails.jsx

--- src/components/AccountDetails.jsx
+++ src/components/AccountDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useWatchlist } from '../components/WatchlistContext';
 
 const AccountDetails = () => {
