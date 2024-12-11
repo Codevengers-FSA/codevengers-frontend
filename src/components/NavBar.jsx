354612653f7dc2363f00e69a16335a2fbb1fa880
Umaid Ismail <umaidismail@umaids-air.mynetworksettings.com>
2024-12-11 08:00:44 -0800

    add css to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,19 +3,12 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav style={{ padding: "10px", backgroundColor: "#f5f5f5" }}>
-      <Link to="/" style={{ margin: "0 10px" }}>Home</Link>
-      <Link to="/moviecatalog" style={{ margin: "0 10px" }}>Movies</Link>
-      <Link to="/watchlists" style={{ margin: "0 10px" }}>Watchlists</Link>
-      <Link to= "/account" style={{margin: "0 10px"}}>Account</Link>
-
-
-      <input 
-        type="text" 
-        placeholder="Search..."
-      />
-
-    </nav>
+    <nav>
+    <Link to="/">Home</Link>
+    <Link to="/moviecatalog">Movies</Link>
+    <Link to="/watchlists">Watchlists</Link>
+    <Link to="/account">Account</Link>
+  </nav>
   );
 }
 
