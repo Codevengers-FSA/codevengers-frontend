201e7d5f49e031575d0971a28385eb0ef6a5c118
Sophia <opyrose12@yahoo.com>
2024-12-16 17:29:44 -0500

    added a nightmode

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
 
 import Home from './components/Homepage';
 import Navbar from './components/NavBar';
@@ -10,20 +9,24 @@ import Account from './components/Account';
 import AccountDetails from './components/AccountDetails';
 import RecommendedMoviesForm from './components/RecommendedMoviesForm';
 
+
 const App = () => {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/moviecatalog" element={<AllMovies />} />
-        <Route path="/moviecatalog/:id" element={<MovieDetails />} />
-        <Route path="/watchlists" element={<Watchlists />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/account-details" element={<AccountDetails />} />
-        <Route path="/recommended-movies" element={<RecommendedMoviesForm />} />
-      </Routes>
+   
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/moviecatalog" element={<AllMovies />} />
+      <Route path="/moviecatalog/:id" element={<MovieDetails />} />
+      <Route path="/watchlists" element={<Watchlists />} />
+      <Route path="/account" element={<Account />} />
+      <Route path="/account-details" element={<AccountDetails />} />
+      <Route path="/recommended-movies" element={<RecommendedMoviesForm />} />
+    </Routes>
+    
     </>
+    
   );
 };
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,21 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom"; 
+import NightModeToggle from "./NightmodeToggle";
 
 const Navbar = () => {
+  const [isNightMode, setIsNightMode] = useState(false);
+
+  const handleToggleTheme = (newMode) => {
+    setIsNightMode(newMode);
+  };
   return (
     <nav>
     <Link to="/">Home</Link>
     <Link to="/moviecatalog">Movies</Link>
     <Link to="/watchlists">Watchlists</Link>
     <Link to="/account">Account</Link>
+
+    <NightModeToggle onToggle={handleToggleTheme} />
   </nav>
   );
 }
