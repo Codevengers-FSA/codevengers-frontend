6425579a37c6de6330b99541f7a78eb463c9f611
Sophia <opyrose12@yahoo.com>
2024-12-16 08:15:58 -0500

    added delete method to watched

--- src/components/AccountDetails.jsx
+++ src/components/AccountDetails.jsx
@@ -121,9 +121,28 @@ const AccountDetails = () => {
     navigate(`/moviecatalog/${movieId}`);
   };
 
-  const removeFromWatchedMovies = (movieId) => {
-    setWatchedMovieIds(prevIds => prevIds.filter(id => id !== movieId));
-    setWatchedMovies(prevMovies => prevMovies.filter(movie => movie.id !== movieId));
+  const removeFromWatchedMovies = async (movieId) => {
+    try {
+      const token = localStorage.getItem('token');
+      const response = await fetch(`https://codevengers-backend.onrender.com/users/${username}/watched/${movieId}`, 
+        {
+          method: 'DELETE',
+          headers: {
+            'Authorization' : `Bearer ${token}`,
+            'Contetnt-Type': `application/json`,
+          },
+        }
+      );
+      if (!response.ok){
+        throw new Error('Failed to remove movie from watched list');
+      }
+      setWatchedMovieIds(prevIds => prevIds.filter(id => id !== movieId));
+      setWatchedMovies(prevMovies => prevMovies.filter(movie => movie.id !== movieId));
+      console.log(`Movie with ${movieId} removed from watched list`);
+    } catch (error) {
+      console.error('Error removing the movie from watched list:', error);
+      alert('Failed to remove the movie, please try later.')
+    }
   };
 
   const handleLogout = () => {
