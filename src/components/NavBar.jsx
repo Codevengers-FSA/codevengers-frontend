3542a29c96d2887d76da3d1ee5b5f380a30cd77a
kdonovan628 <kelsey.donovan628@gmail.com>
2024-12-16 13:32:46 -0700

    updated navbar styling and moved logout button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('username');
+    navigate('/account');
+  };
+
+  const isLoggedIn = !!localStorage.getItem('token');
+
   return (
     <nav>
-    <Link to="/">Home</Link>
-    <Link to="/moviecatalog">Movies</Link>
-    <Link to="/watchlists">Watchlists</Link>
-    <Link to="/account">Account</Link>
-  </nav>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/moviecatalog">Movies</Link>
+        <Link to="/watchlists">Watchlists</Link>
+        <Link to="/account">Account</Link>
+      </div>
+      
+      {isLoggedIn && <button className="logout-button" onClick={handleLogout}>Logout</button>}
+    </nav>
   );
 }
 
